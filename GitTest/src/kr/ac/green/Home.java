817e694bd1158c8917caa2abfe113a76e107c487
arvarta <arvarta@gmail.com>
2024-11-22 16:25:10 +0900

    first test

--- GitTest/src/kr/ac/green/Home.java
+++ GitTest/src/kr/ac/green/Home.java
@@ -3,6 +3,7 @@ package kr.ac.green;
 public class Home {
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("hello second");
 		System.out.println("hello first");
 	}
 }
